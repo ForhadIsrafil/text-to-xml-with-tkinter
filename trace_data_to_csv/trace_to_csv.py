1e3192dca92e08801298705681960cf37ec8e8f4
forhadisrafil <forhadtuhin5088@gmail.com>
2024-04-30 01:20:34 +0600

    new updates.

--- trace_data_to_csv/trace_to_csv.py
+++ trace_data_to_csv/trace_to_csv.py
@@ -9,8 +9,9 @@ from flet import Text, Column, Row, Container, View, AppBar
 from flet import RouteChangeEvent, ViewPopEvent, MainAxisAlignment, CrossAxisAlignment
 import os
 
-file_path_text_box = ft.TextField("Selected Trace Folder Path:", read_only=True, width=600)
-destination_folder_text_box = ft.TextField("Destination Folder Path:", read_only=True, width=600)
+file_path_text_box = ft.TextField("Select Trace Folder Path(*.trace)", read_only=True, width=580)
+trace_destination_folder_text_box = ft.TextField("Trace Destination Folder Path", read_only=True, width=580)
+csv_destination_folder_text_box = ft.TextField("CSV Destination Folder Path", read_only=True, width=580)
 
 status = Text("")
 
@@ -18,7 +19,19 @@ pr = ft.ProgressBar(width=500, border_radius=5, scale=5)
 pr.color = "#FF5733"
 pr.bgcolor = "#96DED1"
 
+
 def generate_csv():
+    if len(glob(file_path_text_box.value + "\*.trace")) == 0:
+        status.value = "No trace files found/directoy not selected."
+        status.color = "#FF5733"
+        status.size = 20
+        status.update()
+        time.sleep(3.5)
+
+        status.value = ''
+        status.color = None
+        status.update()
+
     for file_path in glob(file_path_text_box.value + "\*.trace"):
         # update status and progress view
         status.value = ''
@@ -37,8 +50,8 @@ def generate_csv():
             df.columns = columns = ["@1|DateStamp (YYYY-MM-DD)", "@2|TimeStamp (hh:mm:ss) (24h system)",
                                     "@3|MP (NLUD/SKVR/DRO/USPB)",
                                     "@4|FLXPANELID", "@5|FLXSUBPANELID", "@6|FLXUID", "@7|Result"]
-            df.to_csv(f"{destination_folder_text_box.value}\\{file_name}.csv", index=False)
-            shutil.move(file_path, destination_folder_text_box.value)
+            df.to_csv(f"{csv_destination_folder_text_box.value}\\{file_name}.csv", index=False)
+            shutil.move(file_path, trace_destination_folder_text_box.value)
 
             pr.value = 100
             pr.update()
@@ -62,17 +75,20 @@ def on_dialog_result(e: ft.FilePickerResultEvent):
         file_path_text_box.data = None
         file_path_text_box.update()
 
-    if destination_folder_text_box.data == "destination_folder":
-        destination_folder_text_box.value = e.path
-        destination_folder_text_box.data = None
-        destination_folder_text_box.update()
+    if trace_destination_folder_text_box.data == "trace_destination_folder":
+        trace_destination_folder_text_box.value = e.path
+        trace_destination_folder_text_box.data = None
+        trace_destination_folder_text_box.update()
 
-    # generate_csv(e.files[0])
+    if csv_destination_folder_text_box.data == "csv_destination_folder":
+        csv_destination_folder_text_box.value = e.path
+        csv_destination_folder_text_box.data = None
+        csv_destination_folder_text_box.update()
 
 
 def home(page: ft.Page):
-    page.window_width = 800
-    page.window_height = 400
+    page.window_width = 810
+    page.window_height = 450
     page.window_resizable = False
     file_picker = ft.FilePicker(on_result=on_dialog_result)
     page.overlay.append(file_picker)
@@ -84,17 +100,24 @@ def home(page: ft.Page):
         if folder_name == "trace_folder":
             file_path_text_box.data = folder_name
             file_path_text_box.update()
-            file_picker.get_directory_path(dialog_title="Select Trace Folder")
+            file_picker.get_directory_path(dialog_title="Select Trace Folder (*.trace)")
 
-        if folder_name == "destination_folder":
-            destination_folder_text_box.data = folder_name
-            destination_folder_text_box.update()
-            file_picker.get_directory_path(dialog_title="Select Destination Folder")
+        if folder_name == "trace_destination_folder":
+            trace_destination_folder_text_box.data = folder_name
+            trace_destination_folder_text_box.update()
+            file_picker.get_directory_path(dialog_title="Select Trace Destination Folder")
+
+        if folder_name == "csv_destination_folder":
+            csv_destination_folder_text_box.data = folder_name
+            csv_destination_folder_text_box.update()
+            file_picker.get_directory_path(dialog_title="Select CSV Destination Folder")
 
     choose_trace_folder = ft.ElevatedButton("Choose Trace Folder", on_click=lambda _: which_folder("trace_folder"), )
 
-    destination_folder = ft.ElevatedButton("Destination Folder",
-                                           on_click=lambda _: which_folder("destination_folder"))
+    trace_destination_folder = ft.ElevatedButton("Trace Destination Folder",
+                                                 on_click=lambda _: which_folder("trace_destination_folder"))
+    csv_destination_folder = ft.ElevatedButton("CSV Destination Folder",
+                                               on_click=lambda _: which_folder("csv_destination_folder"))
 
     page.add(
         Row(
@@ -109,8 +132,18 @@ def home(page: ft.Page):
         ),
         Row(
             controls=[
-                destination_folder_text_box,
-                destination_folder,
+                trace_destination_folder_text_box,
+                trace_destination_folder,
+
+            ],
+            # spacing=5,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+        ),
+        Row(
+            controls=[
+                csv_destination_folder_text_box,
+                csv_destination_folder,
 
             ],
             # spacing=5,
